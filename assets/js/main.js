4d6c0583957b885f0f13bc1c2a91b59068a11a32
Kurresailakshmareddy <Kurresailakshmareddy@gmail.com>
2023-11-28 11:22:39 +0000

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -170,8 +170,8 @@ $(function () {
 
 /*===============loader Type animation ===========================*/
 var typed = new Typed(".auto-type", {
-    strings: ["&lt;h4&gt;Loding....!&lt;/h4&gt;", "&lt;h4&gt; Hello World 🌍...!&lt;/h4&gt;"],
-    typeSpeed: 50,
+    strings: ["&lt;h4&gt; Loading.... &lt;/h4&gt;", "&lt;h4&gt; Hello World 🌍...!&lt;/h4&gt;"],
+    typeSpeed: 40,
     backSpeed: 40,
     loop: true
 })
