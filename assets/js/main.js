11aa750da79971c362cdd58bb622f5c7d8a45ca1
Kurresailakshmareddy <Kurresailakshmareddy@gmail.com>
2023-11-28 10:20:59 +0000

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -129,19 +129,19 @@ themeButton.addEventListener('click', () => {
 })
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
-const sr = ScrollReveal({
-    origin: 'top',
-    distance: '60px',
-    duration: 3000,
-    delay: 400
-    //reset: true // Animations repeat
-})
+// const sr = ScrollReveal({
+//     origin: 'top',
+//     distance: '60px',
+//     duration: 3000,
+//     delay: 400
+//     //reset: true // Animations repeat
+// })
 
-sr.reveal(`.home__perfil, .about__image, .contact__mail`, { origin: 'right' })
-sr.reveal(`.home__name, .home__info, 
-            .about__container .section__title-1, .about__info, 
-            .contact__social, .contact__data`, { origin: 'left' })
-sr.reveal(`.skills__card, .projects__card`, { interval: 100 })
+// sr.reveal(`.home__perfil, .about__image, .contact__mail`, { origin: 'right' })
+// sr.reveal(`.home__name, .home__info, 
+//             .about__container .section__title-1, .about__info, 
+//             .contact__social, .contact__data`, { origin: 'left' })
+// sr.reveal(`.skills__card, .projects__card`, { interval: 100 })
 
 /*===============loader animation ===========================*/
 
