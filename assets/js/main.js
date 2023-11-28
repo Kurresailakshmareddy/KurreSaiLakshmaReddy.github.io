fa4db9b639d5ebda5255f466dcfcff7df829c53f
Kurresailakshmareddy <Kurresailakshmareddy@gmail.com>
2023-11-28 10:16:46 +0000

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -145,36 +145,36 @@ sr.reveal(`.skills__card, .projects__card`, { interval: 100 })
 
 /*===============loader animation ===========================*/
 
-// $(function () {
+$(function () {
 
-//     $(window).on("load", function () {
+    $(window).on("load", function () {
 
-//         //hide after 2.8 seconds when window load 
-//         setTimeout(function () {
-//             $(".one").hide();
-//         }, 3000);
+        //hide after 2.8 seconds when window load 
+        setTimeout(function () {
+            $(".one").hide();
+        }, 3000);
 
-//         // after showing second span 
-//         setTimeout(function () {
-//             $(".two").show();
-//         }, 3000);
+        // after showing second span 
+        setTimeout(function () {
+            $(".two").show();
+        }, 3000);
 
-//         //after the hide loader and wrapper class is showing
-//         setTimeout(function () {
-//             $(".loader").fadeOut();
-//             $(".wrapper").show();
-//         }, 6150); //2800 +2800 +50 extra = 5650
-//     });
+        //after the hide loader and wrapper class is showing
+        setTimeout(function () {
+            $(".loader").fadeOut();
+            $(".wrapper").show();
+        }, 6150); //2800 +2800 +50 extra = 5650
+    });
 
-// });
+});
 
 /*===============loader Type animation ===========================*/
-// var typed = new Typed (".auto-type", {
-//     strings:["&lt;h4&gt; Hello World 🌍...!&lt;/h4&gt;"," &lt;h4&gt;Welcome To My Portfolio...😊!&lt;/h4&gt;"],
-//     typeSpeed:40,
-//     backSpeed:40,
-//     loop:true
-// })
+var typed = new Typed (".auto-type", {
+    strings:["&lt;h4&gt; Hello World 🌍...!&lt;/h4&gt;"," &lt;h4&gt;Welcome To My Portfolio...😊!&lt;/h4&gt;"],
+    typeSpeed:40,
+    backSpeed:40,
+    loop:true
+})
 
 /*===============loader Type animation ===========================*/
 var typed = new Typed (".autotype", {
